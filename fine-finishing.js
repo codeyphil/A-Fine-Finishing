81dcf5152d6b031c4b468d7b258bc1f8624d66dc
Phil Johnson <codeyphil@gmail.com>
2016-02-05 02:02:19 -0500

    Drastic overhaul to all pages. Redesigned layout and color scheme. New nav.

--- fine-finishing.js
+++ fine-finishing.js
@@ -1,79 +1,32 @@
 $(document).ready(function() {
+    
+	function bindNavbar() {
+		if ($(window).width() > 767) {
+			$('.navbar-default .dropdown').on('mouseover', function(){
+				$('.dropdown-toggle', this).next('.dropdown-menu').show();
+			}).on('mouseout', function(){
+				$('.dropdown-toggle', this).next('.dropdown-menu').hide();
+			});
+			
+			$('.dropdown-toggle').click(function() {
+				if ($(this).next('.dropdown-menu').is(':visible')) {
+					window.location = $(this).attr('href');
+				}
+			});
+		}
+		else {
+			$('.navbar-default .dropdown').off('mouseover').off('mouseout');
+		}
+	}
 	
+	$(window).resize(function() {
+		bindNavbar();
+	});
+	
+	bindNavbar();
 });
 
-(function($) {
 
-  $.fn.menumaker = function(options) {
-      
-      var cssmenu = $(this), settings = $.extend({
-        title: "Menu",
-        format: "dropdown",
-        sticky: false
-      }, options);
-
-      return this.each(function() {
-        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-        $(this).find("#menu-button").on('click', function(){
-          $(this).toggleClass('menu-opened');
-          var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) { 
-            mainmenu.hide().removeClass('open');
-          }
-          else {
-            mainmenu.show().addClass('open');
-            if (settings.format === "dropdown") {
-              mainmenu.find('ul').show();
-            }
-          }
-        });
-
-        cssmenu.find('li ul').parent().addClass('has-sub');
-
-        multiTg = function() {
-          cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-          cssmenu.find('.submenu-button').on('click', function() {
-            $(this).toggleClass('submenu-opened');
-            if ($(this).siblings('ul').hasClass('open')) {
-              $(this).siblings('ul').removeClass('open').hide();
-            }
-            else {
-              $(this).siblings('ul').addClass('open').show();
-            }
-          });
-        };
-
-        if (settings.format === 'multitoggle') multiTg();
-        else cssmenu.addClass('dropdown');
-
-        if (settings.sticky === true) cssmenu.css('position', 'fixed');
-
-        resizeFix = function() {
-          if ($( window ).width() > 768) {
-            cssmenu.find('ul').show();
-          }
-
-          if ($(window).width() <= 768) {
-            cssmenu.find('ul').hide().removeClass('open');
-          }
-        };
-        resizeFix();
-        return $(window).on('resize', resizeFix);
-
-      });
-  };
-})(jQuery);
-
-(function($){
-$(document).ready(function(){
-
-$("#cssmenu").menumaker({
-   title: "Menu",
-   format: "multitoggle"
-});
-$("#owl-example").owlCarousel();
-});
-})(jQuery);
 
 
 
